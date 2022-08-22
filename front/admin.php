27bc851cd7e9353135914012fc4b9045ad4a8c21
mackliu <mackliu@gmail.com>
2022-08-22 15:44:00 +0800

    完成管理者登入功能

--- front/admin.php
+++ front/admin.php
@@ -1,3 +1,4 @@
+
 <table class="all">
     <tr>
         <td class="tt ct">帳號</td>
@@ -9,7 +10,14 @@
     </tr>
     <tr>
         <td class="tt ct">驗證碼</td>
-        <td class="pp"><input type="text" name="code" id="code"></td>
+        <td class="pp">
+        <?php
+            $a=rand(10,99);
+            $b=rand(10,99);
+            echo $a . " + " . $b . " = ";
+            $_SESSION['ans']=$a+$b;
+        ?>    
+        <input type="text" name="code" id="code"></td>
     </tr>
 </table>
-<div class="ct"><button>確認</button></div>
+<div class="ct"><button onclick="login('admin')">確認</button></div>
