ce19a565f5eb7ea375a7e3c2a4d8e2f93f67631c
mackliu <mackliu@gmail.com>
2022-08-22 11:51:07 +0800

    建立base.php檔案,完成後台頁尾版權編輯功能

--- back/bot.php
+++ back/bot.php
@@ -1,10 +1,19 @@
+<?php
+if(!empty($_POST)){
+    $bot=$Bot->find(1);
+    $bot['bot']=$_POST['bot'];
+    $Bot->save($bot);
+}
+
+?>
+
 <h2 class="ct">編輯頁尾版權區</h2>
-<form action="?" method="post">
+<form action="?do=bot" method="post">
 
 <table class="all">
     <tr>
         <td class="tt ct">頁尾宣告內容</td>
-        <td class="pp"><input type="text" name="bot" value=""></td>
+        <td class="pp"><input type="text" name="bot" value="<?=$Bot->find(1)['bot'];?>"></td>
     </tr>
 </table>
     <div class="ct">
