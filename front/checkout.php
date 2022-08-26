70cbc11e26eade386f226e0c990c469f6941b2a7
mackliu <mackliu@gmail.com>
2022-08-26 14:35:18 +0800

    完成結帳存入訂單功能

--- front/checkout.php
+++ front/checkout.php
@@ -52,6 +52,21 @@ $mem=$Mem->find(['acc'=>$_SESSION['mem']]);
 </table>
 <div class="all ct tt">總價:<?=$sum;?></div>
 <div class="ct">
-    <button onclick="checout()">確定送出</button>
+    <button onclick="checkout()">確定送出</button>
     <button onclick="location.href='?do=buycart'">返回修改訂單</button>
 </div>
+<script>
+    function checkout(){
+        let form={
+            name:$("#name").val(),
+            email:$("#email").val(),
+            addr:$("#addr").val(),
+            tel:$("#tel").val(),
+            total:<?=$sum;?>,
+        }
+        $.post("./api/save_order.php",form,(res)=>{
+            
+            alert("訂購成功\n感謝您的選購")
+        })
+    }
+</script>
