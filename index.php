385a4e1cdc8103bffd0b4adb06a62daf64939189
mackliu <mackliu@gmail.com>
2022-08-26 09:25:17 +0800

    分類選單連結加上分類的ID

--- index.php
+++ index.php
@@ -50,23 +50,23 @@
         <div id="left" class="ct">
             <div style="min-height:400px;">
                 <div class='ww'>
-                    <a href="#">全部商品(8)</a>
+                    <a href="?type=0">全部商品(8)</a>
                 </div>
                 <?php
                 $bigs = $Type->all(['parent' => 0]);
                 foreach ($bigs as $big) {                
                     echo " <div class='ww'>";
-                    echo "<a>{$big['name']}</a>";
+                    echo "<a href='?type={$big['id']}'>{$big['name']}</a>";
                     
                         $mids = $Type->all(['parent' => $big['id']]);
                         if (!empty($mids)) {
                             foreach ($mids as $mid) {
                                 echo "<div class='s'>";
-                                echo "    <a style='background:lightgreen;color:darkgreen'>{$mid['name']}</a>";
+                                echo "    <a href='?type={$mid['id']}' style='background:lightgreen;color:darkgreen'>{$mid['name']}</a>";
                                 echo "</div>";
                             }
                         }
-                        
+
                     echo "</div>";
                 }
                 ?>
